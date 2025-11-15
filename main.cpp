e4d8347db328ae70b541c8083be5a225eea617a9
EduMFreire <eduardodemendoncafreire@gmail.com>
2025-11-15 11:46:55 -0300

    Testa se o dijkstra ainda funciona.
    Reorganiza a parte 1 do trabalho, para consistência. Adiciona um txt com coisas a melhorar.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@
 #include <fstream>
 #include <random>
 
+template <typename T>
+void print_vector(const std::vector<T>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << "\n";
+}
+
 auto time_now() {
     return std::chrono::steady_clock::now();
 }
@@ -17,9 +25,17 @@ double time_elapsed(std::chrono::steady_clock::time_point start, std::chrono::st
 int main() {
     WeightedGraph wg1("Grafos/grafo_com_pesos.txt", true, false);
     WeightedGraph wg2("Grafos/grafo_com_pesos.txt", false, false);
+    WeightedGraph wg3("Grafos/grafo_cpesos2.txt", false, false);
 
     wg1.print();
     wg2.print();
+    wg3.print();
+
+    std::vector<double> dists;
+    std::vector<int> parents;
+
+    wg3.dijkstra(1, dists, parents, false);
+    print_vector(dists);
 
     return 0;
 }
